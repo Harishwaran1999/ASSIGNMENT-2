4368e5af5ff82146477888a73b450b510c5defb1
Harishwaran1999 <126972758+Harishwaran1999@users.noreply.github.com>
2023-03-12 20:54:06 +0530

    Add files via upload

---
+++ ascii.py
@@ -0,0 +1,4 @@
+str =('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+print (str)
+for char in str:
+    print(char,ord(char))
---
+++ ascii2.py
@@ -0,0 +1,4 @@
+str = input("enter a alphabet")
+print (str)
+for char in str:
+    print(char,ord(char))
